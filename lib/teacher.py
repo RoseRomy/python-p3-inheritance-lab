701199137f43e2db9b11fa99994e4122db2bac72
Rosemary Kamau <kamaurosemary.n@gmail.com>
2025-05-13 10:19:34 +0300

    Initial commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
+
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [random.randint(1, 100) for _ in range(15)]
 
     def teach(self):
-        pass
+        if self.knowledge:
+            random_knowledge = random.choice(self.knowledge)
+            return f"Teaching about {random_knowledge}..."
+        else:
+            return "Nothing to teach right now."
